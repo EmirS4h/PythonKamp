25360fe84c623d6acbcdda56023e4fee9fd526a3
Emir <96378991+EmirS4h@users.noreply.github.com>
2023-03-31 21:04:11 +0300

    Hafta 4 - Odev 2 Tamamlandı

--- hafta_4/Odev2/test_saucedemo.py
+++ hafta_4/Odev2/test_saucedemo.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from datetime import date
 import pytest
 import csv
+import constants
 
 
 class Test_SauceDemo:
@@ -20,16 +21,19 @@ class Test_SauceDemo:
 
     # her testten önce calısır
     def setup_method(self):
-        self.standard_user = "standard_user"
-        self.locked_out_user = "locked_out_user"
-        self.password = "secret_sauce"
-        self.driver.get("https://www.saucedemo.com/")
+        self.standard_user = constants.STANDARD_USER
+        self.locked_out_user = constants.LOCKED_OUT_USER
+        self.password = constants.PASSWORD
+        self.driver.get(constants.URL)
         self.actions = ActionChains(self.driver)
         WebDriverWait(self.driver, 5).until(
-            expected_conditions.visibility_of_element_located((By.ID, "user-name")))
-        self.username_input = self.driver.find_element(By.ID, "user-name")
-        self.password_input = self.driver.find_element(By.ID, "password")
-        self.login_btn = self.driver.find_element(By.ID, "login-button")
+            expected_conditions.visibility_of_element_located((By.ID, constants.USERNAME_ID)))
+        self.username_input = self.driver.find_element(
+            By.ID, constants.USERNAME_ID)
+        self.password_input = self.driver.find_element(
+            By.ID, constants.PASSWORD_ID)
+        self.login_btn = self.driver.find_element(
+            By.ID, constants.LOGIN_BTN_ID)
         self.folder_path = str(Path.cwd()) + \
             "\\hafta_4\\Odev2\\" + str(date.today())
         Path(self.folder_path).mkdir(exist_ok=True)
@@ -39,37 +43,58 @@ class Test_SauceDemo:
         self.actions.release()
 
     def login(self):
-        self.username_input.send_keys(self.standard_user)
-        self.password_input.send_keys(self.password)
+        self.username_input.send_keys(constants.STANDARD_USER)
+        self.password_input.send_keys(constants.PASSWORD)
 
         self.login_btn.click()
+
+    def reset_app_state(self):
+        self.driver.find_element(By.ID, constants.MENU_BTN_ID).click()
+        WebDriverWait(self.driver, 5).until(expected_conditions.element_to_be_clickable(
+            (By.ID, constants.RESET_APP_STATE_BTN_ID)))
+        self.driver.find_element(
+            By.ID, constants.RESET_APP_STATE_BTN_ID).click()
 
     def wait(self, element: tuple, max_time: int = 5):
         WebDriverWait(self.driver, max_time).until(
             expected_conditions.visibility_of_element_located(element))
 
-    def get_data():
-        reader = csv.reader("./data/invalid_login.csv")
-        for data in reader:
-            print(data)
+    def get_data(selection: str):
+        with open(r"hafta_4\Odev2\data\login_credentials.csv", "r", newline='') as f:
+            reader = csv.DictReader(f, skipinitialspace=True)
+            data = []
+            match selection:
+                case "username":
+                    for row in reader:
+                        data.append(row["username"])
+                case "password":
+                    for row in reader:
+                        data.append(row["password"])
+                case "both":
+                    for row in reader:
+                        data.append((row["username"], row["password"]))
+        return data
 
-    @pytest.mark.parametrize("password", [("12345678"), ("kullanici adi"), ("bir iki uc dort bes")])
+    def get_checkout_inputs(self):
+        return (self.driver.find_element(By.ID, constants.CHECK_OUT_FIRSTNAME_ID), self.driver.find_element(By.ID, constants.CHECK_OUT_LASTNAME_ID), self.driver.find_element(By.ID, constants.CHECK_OUT_POSTALCODE_ID))
+
+    @pytest.mark.parametrize("password", get_data("password"))
     def test_blank_username_login(self, password):
         self.password_input.send_keys(password)
         self.login_btn.click()
         self.driver.save_screenshot(
             f"{self.folder_path}/blank-username-{password}.png")
-        assert "Epic sadface: Username is required" == self.driver.find_element(
-            By.XPATH, '/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3').text
+        assert constants.BLANK_USERNAME_MESSAGE == self.driver.find_element(
+            By.XPATH, constants.ERROR_MESSAGE_XPATH).text
 
-    @pytest.mark.parametrize("username", [("Emir"), ("Demir"), ("Cebir")])
+    @pytest.mark.parametrize("username", get_data("username"))
     def test_blank_password_login(self, username):
         self.username_input.send_keys(username)
         self.login_btn.click()
         self.driver.save_screenshot(
             f"{self.folder_path}/blank-password-{username}.png")
-        assert "Epic sadface: Password is required" == self.driver.find_element(
-            By.XPATH, '/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3').text
+        assert constants.BLANK_PASSWORD_MESSAGE == self.driver.find_element(
+            By.XPATH, constants.ERROR_MESSAGE_XPATH).text
 
     def test_locked_out_user(self):
         self.actions.send_keys_to_element(
@@ -79,8 +104,8 @@ class Test_SauceDemo:
 
         self.login_btn.click()
         self.driver.save_screenshot(f"{self.folder_path}/locked-out-user.png")
-        assert "Epic sadface: Sorry, this user has been locked out." == self.driver.find_element(
-            By.XPATH, '/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3').text
+        assert constants.LOCKED_OUT_USER_MESSAGE == self.driver.find_element(
+            By.XPATH, constants.ERROR_MESSAGE_XPATH).text
 
     def test_x_icons(self):
         self.login_btn.click()
@@ -89,7 +114,7 @@ class Test_SauceDemo:
         self.driver.save_screenshot(f"{self.folder_path}/x-icons.png")
 
         assert 0 == len(self.driver.find_elements(
-            By.CLASS_NAME, "error_icon"))
+            By.CLASS_NAME, constants.ERROR_ICON_CLASS))
 
     def test_valid_login(self):
         self.actions.send_keys_to_element(
@@ -100,9 +125,9 @@ class Test_SauceDemo:
         self.login_btn.click()
         self.driver.save_screenshot(f"{self.folder_path}/valid-login.png")
         assert 6 == len(self.driver.find_elements(
-            By.CLASS_NAME, "inventory_item"))
+            By.CLASS_NAME, constants.INVENTORY_ITEM_CLASS))
 
-    @pytest.mark.parametrize("username,password", [("Emir", "123456"), ("123456", "Emir")])
+    @pytest.mark.parametrize("username,password", get_data("both"))
     def test_invalid_login(self, username, password):
         self.actions.send_keys_to_element(
             self.username_input, username)
@@ -112,8 +137,8 @@ class Test_SauceDemo:
         self.login_btn.click()
         self.driver.save_screenshot(
             f"{self.folder_path}/invalid-login-{username}-{password}.png")
-        assert "Epic sadface: Username and password do not match any user in this service" == self.driver.find_element(
-            By.XPATH, '/html/body/div/div/div[2]/div[1]/div/div/form/div[3]/h3').text
+        assert constants.INVALID_LOGIN_MESSAGE == self.driver.find_element(
+            By.XPATH, constants.ERROR_MESSAGE_XPATH).text
 
     def test_total_amount(self):
         self.login()
@@ -142,14 +167,14 @@ class Test_SauceDemo:
                 Select(self.driver.find_element(By.CLASS_NAME,
                        "product_sort_container")).select_by_index(0)
                 first_item_name = self.driver.find_element(
-                    By.XPATH, "/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/a/div").text
+                    By.XPATH, constants.FIRST_ITEM_XPATH).text
 
                 assert first_item_name == "Sauce Labs Backpack"
             case "za":
                 Select(self.driver.find_element(By.CLASS_NAME,
                        "product_sort_container")).select_by_index(1)
                 first_item_name = self.driver.find_element(
-                    By.XPATH, "/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[1]/a/div").text
+                    By.XPATH, constants.FIRST_ITEM_XPATH).text
 
                 assert first_item_name == "Test.allTheThings() T-Shirt (Red)"
 
@@ -162,14 +187,14 @@ class Test_SauceDemo:
                 Select(self.driver.find_element(By.CLASS_NAME,
                        "product_sort_container")).select_by_index(2)
                 first_item_price = self.driver.find_element(
-                    By.XPATH, "/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/div").text
+                    By.XPATH, constants.FIRST_ITEM_PRICE_XPATH).text
 
                 assert first_item_price == "$7.99"
             case "hl":
                 Select(self.driver.find_element(By.CLASS_NAME,
                        "product_sort_container")).select_by_index(3)
                 first_item_price = self.driver.find_element(
-                    By.XPATH, "/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/div").text
+                    By.XPATH, constants.FIRST_ITEM_PRICE_XPATH).text
 
                 assert first_item_price == "$49.99"
 
@@ -177,104 +202,110 @@ class Test_SauceDemo:
     def test_shopping_cart_badge(self, num_of_btns_to_click: int):
         self.login()
 
-        self.wait((By.CLASS_NAME, "btn_inventory"))
-        btns = self.driver.find_elements(By.CLASS_NAME, "btn_inventory")
+        self.wait((By.CLASS_NAME, constants.INVENTORY_BUTTON_CLASS))
+
+        btns = self.driver.find_elements(By.CLASS_NAME, constants.INVENTORY_BUTTON_CLASS)
 
         for i in range(num_of_btns_to_click):
             btns[i].click()
 
-        self.driver.save_screenshot(
-            f"{self.folder_path}/shopping-cart-badge.png")
-
-        assert str(num_of_btns_to_click) == self.driver.find_element(
+        badge_number_count = self.driver.find_element(
             By.CLASS_NAME, "shopping_cart_badge").text
 
-    def get_checkout_inputs(self):
-        return (self.driver.find_element(By.ID, "first-name"), self.driver.find_element(By.ID, "last-name"), self.driver.find_element(By.ID, "postal-code"))
+        self.driver.save_screenshot(
+            f"{self.folder_path}/shopping-cart-badge.png")
+        self.driver.refresh()
+
+        btns = self.driver.find_elements(By.CLASS_NAME, constants.INVENTORY_BUTTON_CLASS)
+        for i in range(num_of_btns_to_click):
+            btns[i].click()
+
+        assert str(num_of_btns_to_click) == badge_number_count
 
     def test_empty_checkout_info(self):
         self.login()
 
         self.driver.find_element(
-            By.CLASS_NAME, "shopping_cart_link").click()
-        self.driver.find_element(By.ID, "checkout").click()
-        self.driver.find_element(By.ID, "continue").click()
+            By.CLASS_NAME, constants.SHOPPING_CART_CLASS).click()
+        self.driver.find_element(By.ID, constants.CHECKOUT_BTN_ID).click()
+        self.driver.find_element(By.ID, constants.CONTINUE_BTN_ID).click()
 
         self.driver.save_screenshot(
             f"{self.folder_path}/empty-checkout-info.png")
-        
-        assert 3 == len(self.driver.find_elements(By.CLASS_NAME, "error_icon"))
+
+        assert 3 == len(self.driver.find_elements(
+            By.CLASS_NAME, constants.ERROR_ICON_CLASS))
 
     def test_checkout_empty_firstname(self):
         self.login()
 
         self.driver.find_element(
-            By.CLASS_NAME, "shopping_cart_link").click()
-        self.driver.find_element(By.ID, "checkout").click()
+            By.CLASS_NAME, constants.SHOPPING_CART_CLASS).click()
+        self.driver.find_element(By.ID, constants.CHECKOUT_BTN_ID).click()
         _, last_name, postal_code = self.get_checkout_inputs()
         last_name.send_keys("soyisim")
         postal_code.send_keys("posta-kodu-12345")
 
-        self.driver.find_element(By.ID, "continue").click()
+        self.driver.find_element(By.ID, constants.CONTINUE_BTN_ID).click()
 
         self.driver.save_screenshot(
             f"{self.folder_path}/checkout-empty-firstname.png")
-        
-        assert "Error: First Name is required" == self.driver.find_element(
-            By.XPATH, "/html/body/div/div/div/div[2]/div/form/div[1]/div[4]/h3").text
+
+        assert constants.FIRST_NAME_MESSAGE == self.driver.find_element(
+            By.XPATH, constants.CHECKOUT_ERROR_MESSAGE_XPATH).text
 
     def test_checkout_empty_lastname(self):
         self.login()
 
         self.driver.find_element(
-            By.CLASS_NAME, "shopping_cart_link").click()
-        self.driver.find_element(By.ID, "checkout").click()
+            By.CLASS_NAME, constants.SHOPPING_CART_CLASS).click()
+        self.driver.find_element(By.ID, constants.CHECKOUT_BTN_ID).click()
         first_name, _, postal_code = self.get_checkout_inputs()
         first_name.send_keys("emir")
         postal_code.send_keys("posta-kodu-12345")
 
-        self.driver.find_element(By.ID, "continue").click()
+        self.driver.find_element(By.ID, constants.CONTINUE_BTN_ID).click()
 
         self.driver.save_screenshot(
             f"{self.folder_path}/checkout-empty-lastname.png")
 
-        assert "Error: Last Name is required" == self.driver.find_element(
-            By.XPATH, "/html/body/div/div/div/div[2]/div/form/div[1]/div[4]/h3").text
+        assert constants.LASTNAME_MESSAGE == self.driver.find_element(
+            By.XPATH, constants.CHECKOUT_ERROR_MESSAGE_XPATH).text
 
     def test_checkout_empty_postal_code(self):
         self.login()
 
         self.driver.find_element(
-            By.CLASS_NAME, "shopping_cart_link").click()
-        self.driver.find_element(By.ID, "checkout").click()
+            By.CLASS_NAME, constants.SHOPPING_CART_CLASS).click()
+        self.driver.find_element(By.ID, constants.CHECKOUT_BTN_ID).click()
         first_name, last_name, _ = self.get_checkout_inputs()
         first_name.send_keys("emir")
         last_name.send_keys("soyisim")
 
-        self.driver.find_element(By.ID, "continue").click()
+        self.driver.find_element(By.ID, constants.CONTINUE_BTN_ID).click()
 
         self.driver.save_screenshot(
             f"{self.folder_path}/checkout-empty-postalcode.png")
 
-        assert "Error: Postal Code is required" == self.driver.find_element(
-            By.XPATH, "/html/body/div/div/div/div[2]/div/form/div[1]/div[4]/h3").text
+        assert constants.POSTAL_CODE_MESSAGE == self.driver.find_element(
+            By.XPATH, constants.CHECKOUT_ERROR_MESSAGE_XPATH).text
 
     def test_valid_checkout_info(self):
         self.login()
 
         self.driver.find_element(
-            By.CLASS_NAME, "shopping_cart_link").click()
-        self.driver.find_element(By.ID, "checkout").click()
+            By.CLASS_NAME, constants.SHOPPING_CART_CLASS).click()
+        self.driver.find_element(By.ID, constants.CHECKOUT_BTN_ID).click()
         first_name, last_name, postal_code = self.get_checkout_inputs()
         first_name.send_keys("emir")
         last_name.send_keys("soyisim")
         postal_code.send_keys("posta-kodu-12345")
 
-        self.driver.find_element(By.ID, "continue").click()
+        self.driver.find_element(By.ID, constants.CONTINUE_BTN_ID).click()
 
         self.driver.save_screenshot(
             f"{self.folder_path}/valid-checkout-info.png")
-        
+
         assert "Checkout: Overview" == self.driver.find_element(
             By.CLASS_NAME, "title").text
 
@@ -282,19 +313,26 @@ class Test_SauceDemo:
     def test_cart_item(self, num_of_items_to_add: int):
         self.login()
 
-        items = self.driver.find_elements(By.CLASS_NAME, "btn_inventory")
+        items = self.driver.find_elements(By.CLASS_NAME, constants.INVENTORY_BUTTON_CLASS)
 
         for i in range(num_of_items_to_add):
             items[i].click()
 
         self.driver.find_element(
-            By.CLASS_NAME, "shopping_cart_link").click()
+            By.CLASS_NAME, constants.SHOPPING_CART_CLASS).click()
 
-        cart_items = self.driver.find_elements(By.CLASS_NAME, "cart_item")
+        cart_items = self.driver.find_elements(By.CLASS_NAME, constants.CART_ITEM_CLASS)
 
         self.driver.save_screenshot(
             f"{self.folder_path}/cart-item.png")
-        
+
+        self.driver.back()
+
+        items = self.driver.find_elements(By.CLASS_NAME, constants.INVENTORY_BUTTON_CLASS)
+
+        for i in range(num_of_items_to_add):
+            items[i].click()
+
         assert num_of_items_to_add == len(cart_items)
 
     def test_number_of_social_links(self):
@@ -306,20 +344,20 @@ class Test_SauceDemo:
     def test_price_after_tax(self):
         self.login()
 
-        items = self.driver.find_elements(By.CLASS_NAME, "btn_inventory")
+        items = self.driver.find_elements(By.CLASS_NAME, constants.INVENTORY_BUTTON_CLASS)
 
         for item in items:
             item.click()
 
         self.driver.find_element(
-            By.CLASS_NAME, "shopping_cart_link").click()
-        self.driver.find_element(By.ID, "checkout").click()
+            By.CLASS_NAME, constants.SHOPPING_CART_CLASS).click()
+        self.driver.find_element(By.ID, constants.CHECKOUT_BTN_ID).click()
         first_name, last_name, postal_code = self.get_checkout_inputs()
         first_name.send_keys("emir")
         last_name.send_keys("soyisim")
         postal_code.send_keys("posta-kodu-12345")
 
-        self.driver.find_element(By.ID, "continue").click()
+        self.driver.find_element(By.ID, constants.CONTINUE_BTN_ID).click()
 
         self.driver.save_screenshot(
             f"{self.folder_path}/price-after-tax.png")
@@ -347,4 +385,4 @@ class Test_SauceDemo:
         self.login()
 
         assert 6 == len(self.driver.find_elements(
-            By.CLASS_NAME, "btn_inventory"))
+            By.CLASS_NAME, constants.INVENTORY_BUTTON_CLASS))
